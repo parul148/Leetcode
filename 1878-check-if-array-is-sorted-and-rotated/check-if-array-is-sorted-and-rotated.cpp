74a1096748aab1d91342272db73faaf40d41a822
Parul Mehra <115182864+parul148@users.noreply.github.com>
2023-09-20 16:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (8.41%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,7 +3,7 @@ public:
     bool check(vector<int>& nums) {
    int count =0;
    int n = nums.size();
-
+ //yha i = 0 isliye ni liya h kyuki 0-1 kerenge to -1 aa jayega jo possible nii h
    for(int i =1; i<n ;i++){
        if(nums[i - 1]> nums[i]){
            count++;
