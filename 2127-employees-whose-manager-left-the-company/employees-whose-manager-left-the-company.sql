2140d8ac83a4853df671197d8594c85919423e8e
Parul Mehra <115182864+parul148@users.noreply.github.com>
2024-04-09 22:30:07 +0530

    Time: 895 ms (15.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT employee_id from Employees
+where manager_id not in 
+(select employee_id from Employees)
+and salary < 30000 order by employee_id ; 
